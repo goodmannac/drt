1e38f7b0f982bed2b49c04348ec080b2ee60bc32
emsimaceae <ekacharmina@ucdavis.edu>
2024-07-27 09:58:56 -0700

    Add files via upload

---
+++ joiningMeta_ST_Taxa_Gels.R
@@ -0,0 +1,27 @@
+#Welcome packages into the script
+library(tidyr)
+library(dplyr)
+
+#Load in both spreadsheets (taxa and metadata) don't forget actions!
+
+metadata <- read.csv("~/Desktop/Bogar Lab/metadatawlocations.csv")
+
+#splicing and rejoining with dashes tree, treatment, and tipID
+taxa <- read.csv("~/Desktop/Bogar Lab/FinalSTtaxon.csv")
+taxa1 = separate(taxa, tip.ID, into = c("treetreatment",  "tip"), sep = "-")
+
+taxa2 = separate(taxa1, treetreatment, into = c("nothing", "tree", "treatment", "silicatreatment"), sep = "")
+
+taxa3 = select(taxa2, (!(nothing)))
+taxa3$silicatreatment = replace_na(taxa3$silicatreatment, "")
+taxa3 = taxa3 %>%unite("silicatreatment", 3:4, sep = "")
+taxa4 = taxa3 %>%unite("tip.ID", 2:4, sep = "-")
+
+#joining gel MGV
+gels <- read.csv("~/Desktop/Bogar Lab/DRTSTgels.csv")
+gels2 = gels %>%unite("tip.ID", 5:7, sep = "-")
+
+md.w.Loc = full_join(metadata, taxa4, by = "tip.ID")
+md.w.Loc1 = full_join(md.w.Loc, gels2, by = "tip.ID")
+
+write.csv(md.w.Loc1, "~/Desktop/Bogar Lab/Meta Data with ST taxa and gels.csv")
